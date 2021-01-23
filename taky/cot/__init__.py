2d982feddc8b8223da61f4f043e29799534003c4
Tim K <tkuester@users.noreply.github.com>
2021-01-23 13:22:12 -0500

    cot: Adding Teams enum

--- taky/cot/__init__.py
+++ taky/cot/__init__.py
@@ -2,3 +2,4 @@ from taky.cot.models import Point, Event, TAKUser, GeoChat
 from taky.cot.server import COTServer
 from taky.cot.client import TAKClient
 from taky.cot.router import COTRouter
+from taky.cot.teams import Teams
---
+++ taky/cot/teams.py
@@ -0,0 +1,17 @@
+import enum
+
+class Teams(enum.Enum):
+    WHITE = 'White'
+    YELLOW = 'Yellow'
+    ORANGE = 'Orange'
+    MAGENTA = 'Magenta'
+    RED = 'Red'
+    MAROON = 'Maroon'
+    PURPLE = 'Purple'
+    DARK_BLUE = 'Dark Blue'
+    BLUE = 'Blue'
+    CYAN = 'Cyan'
+    TEAL = 'Teal'
+    GREEN = 'Green'
+    DARK_GREEN = 'Dark Green'
+    BROWN = 'Brown'
