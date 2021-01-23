df3c692848057b3cb5245bebe6a8111963ba20fe
Tim K <tkuester@users.noreply.github.com>
2021-01-23 17:37:49 -0500

    main: Better argument parsing for default port

--- taky/__main__.py
+++ taky/__main__.py
@@ -8,20 +8,35 @@ from taky.cot import COTServer
 
 def main():
     argp = argparse.ArgumentParser(description="Start the taky server")
-    argp.add_argument('-l', action='store', dest='log_level', default='INFO',
-                      choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+    argp.add_argument('-l', action='store', dest='log_level', default='info',
+                      choices=['debug', 'info', 'warning', 'error', 'critical'],
                       help="Specify log level")
-    argp.add_argument('-n', '--ip', action='store', dest='ip', type=ip_address, default='::')
-    argp.add_argument('-p', '--port', action='store', dest='port', type=int, default=8087)
-    argp.add_argument('--ssl-cert', action='store', dest='ssl_cert', type=str, default=None)
-    argp.add_argument('--ssl-key', action='store', dest='ssl_key', type=str, default=None)
-    argp.add_argument('--cacert', action='store', dest='ca_cert', type=str, default=None)
-    argp.add_argument('--no-verify-client', action='store_false', dest='verify_client', default=True)
+    argp.add_argument('-n', '--ip', action='store', dest='ip', type=ip_address, default='::',
+                      help="IP Address to listen on (v4 or v6)")
+    argp.add_argument('-p', '--port', action='store', dest='port', type=int, default=None,
+                      help="Port to listen on")
+    argp.add_argument('--ssl-cert', action='store', dest='ssl_cert', type=str, default=None,
+                      help="Path to the server certificate")
+    argp.add_argument('--ssl-key', action='store', dest='ssl_key', type=str, default=None,
+                      help="Path to the server certificate key")
+    argp.add_argument('--cacert', action='store', dest='ca_cert', type=str, default=None,
+                      help="Path to the CA for verifying client certs")
+    argp.add_argument('--no-verify-client', action='store_false', dest='verify_client', default=True,
+                      help="Do not verify client certificates")
     argp.add_argument('--version', action='version', version='%%(prog)s version %s' % __version__)
 
     args = argp.parse_args()
 
-    logging.basicConfig(level=args.log_level, stream=sys.stdout)
+    if args.ssl_cert or args.ssl_key:
+        if (args.ssl_cert is None or args.ssl_key is None):
+            argp.error('SSL requires both --ssl-cert and --ssl-key')
+        if args.port is None:
+            args.port = 8089
+    else:
+        if args.port is None:
+            args.port = 8087
+
+    logging.basicConfig(level=args.log_level.upper(), stream=sys.stdout)
 
     try:
         cs = COTServer(ip=args.ip, port=args.port,
