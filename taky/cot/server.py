84a9fe1730d089d923c60f1dfa19c5978036a4fa
Tim K <tkuester@users.noreply.github.com>
2021-02-21 10:19:20 -0500

    cot_server: Massive pylint + doc

--- taky/cot/server.py
+++ taky/cot/server.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# pylint: disable=missing-module-docstring
 import socket
 import select
 import ssl
@@ -8,7 +9,17 @@ from .router import COTRouter
 from .client import TAKClient
 
 class COTServer:
+    '''
+    COTServer is an object which hosts the server socket, handles client
+    sockets, and routes packets between them.
+
+    In the simplest usage, create the object, and call loop()
+    '''
+
     def __init__(self, config):
+        '''
+        Construct the COTServer object, and build the server socket
+        '''
         self.lgr = logging.getLogger(self.__class__.__name__)
 
         self.config = config
@@ -16,26 +27,31 @@ class COTServer:
         self.router = COTRouter()
 
         self.srv = None
-        self.sock_setup()
+        self._sock_setup()
 
-    def sock_setup(self):
-        ip = self.config.get('taky', 'bind_ip')
+    def _sock_setup(self):
+        '''
+        Build the server socket
+        '''
+        ip_addr = self.config.get('taky', 'bind_ip')
         port = self.config.getint('cot_server', 'port')
 
-        if ip is None:
-            ip = ''
+        if ip_addr is None:
+            ip_addr = ''
             sock_fam = socket.AF_INET
             bind_args = ('', port)
         else:
             try:
-                ai = socket.getaddrinfo(ip, port, type=socket.SOCK_STREAM)
-                if len(ai) > 1:
-                    self.lgr.warning("Multiple address entities for %s:%s", ip, port)
-                (sock_fam, _, _, _, bind_args) = ai[0]
-            except socket.gaierror as e:
-                raise ValueError(f"Unable to determine address info for bind_ip: {ip}") from e
+                addr_info = socket.getaddrinfo(ip_addr, port, type=socket.SOCK_STREAM)
+                if len(addr_info) > 1:
+                    self.lgr.warning("Multiple address entities for %s:%s", ip_addr, port)
+                (sock_fam, _, _, _, bind_args) = addr_info[0]
+            except socket.gaierror as exc:
+                raise ValueError(
+                    f"Unable to determine address info for bind_ip: {ip_addr}"
+                ) from exc
 
-        self.ssl_ctx = self.ssl_setup()
+        self.ssl_ctx = self._ssl_setup()
 
         self.srv = socket.socket(sock_fam, socket.SOCK_STREAM)
         self.srv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -43,10 +59,13 @@ class COTServer:
 
         mode = 'ssl' if self.ssl_ctx else 'tcp'
 
-        self.lgr.info("Listening for %s on %s:%s", mode, ip, port)
+        self.lgr.info("Listening for %s on %s:%s", mode, ip_addr, port)
         self.srv.listen()
 
-    def ssl_setup(self):
+    def _ssl_setup(self):
+        '''
+        Build the SSL context
+        '''
         if not self.config.getboolean('ssl', 'enabled'):
             return None
 
@@ -71,32 +90,30 @@ class COTServer:
             ssl_ctx.load_cert_chain(certfile=self.config.get('ssl', 'cert'),
                                     keyfile=self.config.get('ssl', 'key'),
                                     password=self.config.get('ssl', 'key_pw'))
-        except (ssl.SSLError, OSError) as e:
-            self.lgr.error("Unable to load SSL certificate: %s", e)
-            raise e
+        except (ssl.SSLError, OSError) as exc:
+            self.lgr.error("Unable to load SSL certificate: %s", exc)
+            raise exc
 
         return ssl_ctx
 
     def handle_accept(self):
+        '''
+        Accept a new client on the server socket
+        '''
         try:
             (sock, addr) = self.srv.accept()
             if self.ssl_ctx:
                 sock = self.ssl_ctx.wrap_socket(sock, server_side=True,
                                                 do_handshake_on_connect=False)
                 sock.setblocking(False)
-        except (ssl.SSLError, ssl.SSLWantReadError, ssl.SSLWantWriteError, socket.error, socket.timeout) as e:
-            self.lgr.info("Rejecting client: %s", e)
-            return
-        except OSError as e:
-            # https://bugs.python.org/issue31122
-            if e.errno != 0:
-                self.lgr.warning("Unable to accept client: %s", e)
+        except (ssl.SSLError, socket.error, OSError) as exc:
+            self.lgr.info("Rejecting client: %s", exc)
             return
 
-        (ip, port) = addr[0:2]
-        self.lgr.info("New client from %s:%s", ip, port)
+        (ip_addr, port) = addr[0:2]
+        self.lgr.info("New client from %s:%s", ip_addr, port)
         self.clients[sock] = TAKClient(
-            ip,
+            ip_addr,
             port,
             self.router,
             cot_log_dir=self.config.get('cot_server', 'log_cot')
@@ -106,6 +123,11 @@ class COTServer:
         self.router.client_connect(self.clients[sock])
 
     def client_rx(self, sock):
+        '''
+        Receive data from client socket, and feed to TAKClient
+
+        Also responsible for preforming the SSL handshake
+        '''
         client = self.clients[sock]
         # client.ssl_hs is hacky
         if self.ssl_ctx and client.ssl_hs is not True:
@@ -119,8 +141,8 @@ class COTServer:
             except ssl.SSLWantWriteError:
                 # This should never happen, but let's catch it anyways
                 client.ssl_hs = 'tx'
-            except (ssl.SSLError, OSError) as e:
-                self.client_disconnect(sock, str(e))
+            except (ssl.SSLError, OSError) as exc:
+                self.client_disconnect(sock, str(exc))
 
             return
 
@@ -132,25 +154,36 @@ class COTServer:
                 return
 
             client.feed(data)
-        except (socket.error, IOError, OSError) as e:
-            self.client_disconnect(sock, str(e))
+        except (socket.error, IOError, OSError) as exc:
+            self.client_disconnect(sock, str(exc))
 
     def client_tx(self, sock):
+        '''
+        Transmit data to client socket
+
+        If the client is SSL enabled, and the handshake has not yet taken
+        place, we fail silently.
+        '''
         client = self.clients[sock]
-        if self.ssl_ctx and client.ssl_hs == 'tx':
-            self.client_rx(sock)
+        if self.ssl_ctx:
+            if client.ssl_hs is not True:
+                self.client_rx(sock)
+
             return
 
         try:
             sent = sock.send(client.out_buff[0:4096])
             client.out_buff = client.out_buff[sent:]
-        except (socket.error, IOError, OSError) as e:
-            self.client_disconnect(sock, str(e))
+        except (socket.error, IOError, OSError) as exc:
+            self.client_disconnect(sock, str(exc))
 
     def client_disconnect(self, sock, reason=None):
+        '''
+        Disconnect a client from the server
+        '''
         try:
             sock.shutdown(socket.SHUT_RDWR)
-        except:
+        except: # pylint: disable=bare-except
             pass
         sock.close()
 
@@ -163,6 +196,9 @@ class COTServer:
         client.close()
 
     def loop(self):
+        '''
+        Main loop. Call outside this object in a "while True" block.
+        '''
         rd_clients = [self.srv]
         wr_clients = []
         for (sock, client) in self.clients.items():
