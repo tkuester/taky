94577e195d354a061295b01a43f1a6fa182a1683
Tim K <tkuester@users.noreply.github.com>
2021-04-02 11:19:14 -0400

    tests: Return more expressive ValueError

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,13 +3,15 @@ def elements_equal(e1, e2):
     Returns True if two elements are identical
     """
     if e1.tag != e2.tag:
-        return False
+        raise ValueError(f"Tags differ: {e1.tag} != {e2.tag}")
     if e1.text != e2.text:
-        return False
+        raise ValueError(f"Text differs: '{e1.text}' != '{e2.text}'")
     if e1.tail != e2.tail:
-        return False
+        raise ValueError(f"Tail differs: '{e1.tail}' != '{e2.tail}'")
     if e1.attrib != e2.attrib:
-        return False
+        raise ValueError(f"Attrib differs {e1.tag}.attrib != {e2.tag}.attrib")
     if len(e1) != len(e2):
-        return False
+        raise ValueError(
+            f"Length differs: len({e1.tag})({len(e1)}) != len({e2.tag})({len(e2)})"
+        )
     return all(elements_equal(c1, c2) for c1, c2 in zip(e1, e2))
