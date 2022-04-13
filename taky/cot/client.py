tkuester/taky

commit 0a0f08ba7afb8e2a5bd949cca6bcb7510bd6b2bb

All new clients get persist data on connection now
Previously, we would wait until a client sent some sort of
"identification" packet before sending them data. However, this added
needless complexity to the system, especially for script based clients.

While it is still strongly advised to send an identification packet of
some sort, this requirement has been removed to simplify server code,
and to ease development for script based clients.

taky/cot/client.py
@@ -30,16 +30,20 @@ class SocketClient:
     server, such as SSL handshake state, and an outgoing data buffer.
     """
 
-    def __init__(self, sock, use_ssl=False, **kwargs):
+    def __init__(self, sock, use_ssl=False, connect_cb=None, **kwargs):
         self.sock = sock
         self.ssl = use_ssl
         self.ssl_hs = SSLState.SSL_WAIT if use_ssl else SSLState.NO_SSL
         self.out_buff = b""
+        self.connect_cb = connect_cb
 
         (ip, port) = self.addr
         lgr_name = f"{self.__class__.__name__}@{ip}:{port}"
         self.lgr = logging.getLogger(lgr_name)
         super().__init__(**kwargs)
+
+        if self.ready:
+            self.connect_cb(self)
 
     @property
     def addr(self):
@@ -90,6 +94,7 @@ class SocketClient:
             self.ssl_hs = SSLState.SSL_ESTAB
             self.sock.setblocking(True)
             # TODO: Check SSL certs here
+            self.connect_cb(self)
         except ssl.SSLWantReadError:
             self.ssl_hs = SSLState.SSL_WAIT
         except ssl.SSLWantWriteError:
@@ -297,11 +302,7 @@ class TAKClient:
             return
 
         if isinstance(evt.detail, models.TAKUser):
-            if self.user is None:
-                self.user = evt.detail
-                self.router.client_ident(self)
-            else:
-                self.user = evt.detail
+            self.user = evt.detail
 
     def pong(self):
         """

taky/cot/router.py
@@ -41,6 +41,7 @@ class COTRouter:
         Add a client to the router
         """
         self.clients.add(client)
+        self.send_persist(client)
 
     def client_disconnect(self, client):
         """
@@ -48,9 +49,9 @@ class COTRouter:
         """
         self.clients.discard(client)
 
-    def client_ident(self, client):
+    def send_persist(self, client):
         """
-        Called by TAKClient when the client first identifies to the server
+        Sends persistence objects to the client
         """
         self.lgr.debug("Sending persistence objects to %s", client)
         for event in self.persist.get_all():

taky/cot/server.py
@@ -175,9 +175,11 @@ class COTServer:
             use_ssl=use_ssl,
             router=self.router,
             log_cot_dir=config.get("cot_server", "log_cot"),
+            connect_cb=self.client_connect,
         )
 
-        self.router.client_connect(self.clients[sock])
+    def client_connect(self, client):
+        self.router.client_connect(client)
 
     def client_disconnect(self, client, reason=None):
         """

tests/test_router.py
@@ -70,17 +70,12 @@ class RouterTestcase(ut.TestCase):
         self.router.client_connect(self.tk1)
         self.tk1.feed(self.tk1_ident_msg)
 
-        # TK2 connets, and mock identifies. It should receive info about TK1
+        # TK2 connects -- and should have info about TK1 from persist
         self.router.client_connect(self.tk2)
-        self.router.client_ident(self.tk2)
         ret = self.tk2.queue.get_nowait()
         self.assertTrue(ret.uid == "ANDROID-deadbeef")
 
         # TK1 should not have any packets yet...
-        self.assertRaises(queue.Empty, self.tk1.queue.get_nowait)
-
-        # ...even if it re-mock identifies!
-        self.router.client_ident(self.tk1)
         self.assertRaises(queue.Empty, self.tk1.queue.get_nowait)
 
     def test_geochat(self):

commit 44b15fc67b7b4e458dc497b9d5656f798eaa4123

Fix SSL clients not receiving persistence
This is an ugly bug introduced in 0a0f08b. Because SSL clients were
still mid-handshake, the packets were getting silently dropped. This has
been fixed by using the "connect callback" in SocketClient.

Additionally, I ran into issues with the super() constructor and
multiple inheritance. I'm calling the inheritance explicitly now.

taky/cot/client.py
@@ -31,19 +31,18 @@ class SocketClient:
     server, such as SSL handshake state, and an outgoing data buffer.
     """
 
-    def __init__(self, sock, use_ssl=False, connect_cb=None, **kwargs):
+    def __init__(self, sock, use_ssl=False, **kwargs):
         self.sock = sock
         self.ssl = use_ssl
         self.ssl_hs = SSLState.SSL_WAIT if use_ssl else SSLState.NO_SSL
         self.out_buff = b""
-        self.connect_cb = connect_cb
+        self.connect_cb = kwargs.get("cbs", {}).get("connect", lambda cli: None)
 
         (ip, port) = self.addr
         lgr_name = f"{self.__class__.__name__}@{ip}:{port}"
         self.lgr = logging.getLogger(lgr_name)
-        super().__init__(**kwargs)
 
-        if self.ready and self.connect_cb:
+        if self.ready:
             self.connect_cb(self)
 
     @property
@@ -95,8 +94,7 @@ class SocketClient:
             self.ssl_hs = SSLState.SSL_ESTAB
             self.sock.setblocking(True)
             # TODO: Check SSL certs here
-            if self.connect_cb:
-                self.connect_cb(self)
+            self.connect_cb(self)
         except ssl.SSLWantReadError:
             self.ssl_hs = SSLState.SSL_WAIT
         except ssl.SSLWantWriteError:
@@ -185,17 +183,17 @@ class TAKClient:
     the client.
     """
 
-    def __init__(self, log_cot_dir=None, cbs=None, **kwargs):
+    def __init__(self, log_cot_dir=None, **kwargs):
         self.lgr = logging.getLogger(self.__class__.__name__)
         self.user = None
         self.connected = time.time()
         self.num_rx = 0
         self.last_rx = 0
 
-        if not cbs:
-            cbs = {}
+        cbs = kwargs.get("cbs", {})
         self.route = cbs.get("route", lambda pkt: None)
         self.packet_rx = cbs.get("packet_rx", lambda pkt: None)
+        self.client_ident = cbs.get("client_ident", lambda pkt: None)
 
         self.log_cot_dir = log_cot_dir
         if log_cot_dir and not os.path.exists(log_cot_dir):
@@ -206,8 +204,6 @@ class TAKClient:
         parser = etree.XMLPullParser(tag="event", resolve_entities=False)
         parser.feed(b"<root>")
         self.xdc = XMLDeclStrip(parser)
-
-        super().__init__(**kwargs)
 
     def __repr__(self):
         if self.user:
@@ -333,7 +329,11 @@ class TAKClient:
             return
 
         if isinstance(evt.detail, models.TAKUser):
-            self.user = evt.detail
+            if self.user is None:
+                self.user = evt.detail
+                self.client_ident(self)
+            else:
+                self.user = evt.detail
 
     def pong(self):
         """
@@ -358,7 +358,8 @@ class SocketTAKClient(TAKClient, SocketClient):
     """
 
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+        TAKClient.__init__(self, **kwargs)
+        SocketClient.__init__(self, **kwargs)
 
     def __repr__(self):
         if self.user:

taky/cot/router.py
@@ -41,7 +41,6 @@ class COTRouter:
         Add a client to the router
         """
         self.clients.add(client)
-        self.send_persist(client)
 
     def client_disconnect(self, client):
         """

taky/cot/server.py
@@ -226,6 +226,7 @@ class COTServer:
                 cbs={
                     "route": self.router.route,
                     "packet_rx": self.mon_packet,
+                    "connect": self.router.send_persist,
                 },
             )
 